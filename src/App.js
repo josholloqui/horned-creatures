cfc339f8a8c16c131084ed1360db6165a8fe9700
Frank Josh Olloqui <josh.olloqui@gmail.com>
2020-08-04 18:19:18 -0700

    adds full keyword functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,53 @@
 import React from 'react';
 import Header from './Header.js';
-import CreatureList from './CreatureList.js';
-import creatureData from './data.js';
+import CreaturesList from './CreatureList.js';
+import creaturesData from './data.js';
 import './App.css';
 
-function App() {
-  return (
-    <body>
-      <Header />
-      <CreatureList creatures={creatureData} />
+const keywords = [
+  'narwhal',
+  'rhino',
+  'unicorn',
+  'unilego',
+  'triceratops',
+  'markhor',
+  'mouflon',
+  'addax',
+  'chameleon',
+  'lizard',
+  'dragon'
+]
+
+// const horns = [
+//   1,
+//   2,
+//   3,
+//   100
+// ]
+
+class App extends React.Component {
+  state = { keyword: null }
+  handleKeywordType = (e) => {
+    const keyword = e.target.value;
+
+    this.setState({keyword})
+  }
+  render() {
+    const filteredKeywords = creaturesData.filter(creature => creature.keyword === this.state.keyword || !this.state.keyword);
+    return (
+      <body>
+        <Header />
+        <select onChange={this.handleKeywordType}>
+          <optgroup label="Keyword">
+            {
+              keywords.map(keyword => <option key={keyword} value={keyword}>{keyword}</option>)
+            }
+          </optgroup>
+        </select>
+        <CreaturesList creatures={filteredKeywords} />
     </body>
-  );
+    );
+  }
 }
 
 export default App;
